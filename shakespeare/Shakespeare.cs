ab34cd1282cf7bdc92aa6fd1dd4573b69c42158f
ADRIAN30 <medveadrian.szgya@gmail.com>
2022-10-17 11:55:48 +0200

    create properties

--- shakespeare/Shakespeare.cs
+++ shakespeare/Shakespeare.cs
@@ -8,5 +8,44 @@ namespace shakespeare
 {
     class Shakespeare
     {
+        private string magyarCim;
+
+        public string MagyarCim
+        {
+            get { return magyarCim; }
+            private set { magyarCim = value; }
+        }
+
+        private string angolCim;
+
+        public string AngolCim
+        {
+            get { return angolCim; }
+            private set { angolCim = value; }
+        }
+
+        private int befejezes;
+
+        public int Befejezes
+        {
+            get { return befejezes; }
+            private set { befejezes = value; }
+        }
+
+        private bool komedia;
+
+        public bool Komedia
+        {
+            get { return komedia; }
+            private set { komedia = value; }
+        }
+
+        public Shakespeare(string magyarCim, string angolCim, int befejezes, bool komedia)
+        {
+            MagyarCim = magyarCim;
+            AngolCim = angolCim;
+            Befejezes = befejezes;
+            Komedia = komedia;
+        }
     }
 }
